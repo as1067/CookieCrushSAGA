d700202d5b70e7e06682bc0cb3855db75f9c6832
Cyrus Tau <cyrustau@Babas-iMac.local>
2014-07-09 16:22:59 -0700

    Tutorial 1 Bugs

--- CookieCrushSaga/Array2D.swift
+++ CookieCrushSaga/Array2D.swift
@@ -6,4 +6,23 @@
 //  Copyright (c) 2014 Cyrus Tau. All rights reserved.
 //
 
-import Foundation
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    var array: Array<T?>  // private
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count: rows*columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[row*columns + column]
+        }
+        set {
+            array[row*columns + column] = newValue
+        }
+    }
+}
